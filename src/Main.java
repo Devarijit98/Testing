1465e1277387ce8cb452512bcbe22a88556c6608
Arijit Adhikary <imarijitadhikary.1998@gmail.com>
2024-10-09 14:15:24 +0530

    Fixed

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,11 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Dibendu");
+
+        System.out.println("Dibendu Ghosh");
+
+    }
+    public void add(){
+        System.out.println("Arijit in ");
     }
 }
